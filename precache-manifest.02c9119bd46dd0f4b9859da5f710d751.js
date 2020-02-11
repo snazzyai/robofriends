cbd1ffa2a5a6a08ab13fea5c9a47dff6156ea907
Abdulsalam Ibrahim <engineerabdulsalam0@gmail.com>
2020-02-11 09:14:22 +0500

    Updates

---
+++ precache-manifest.02c9119bd46dd0f4b9859da5f710d751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "22d5ba9cda52dc6f1bc6",
+    "url": "/robofriends/static/js/main.22d5ba9c.chunk.js"
+  },
+  {
+    "revision": "3fe04a6d5aa9c0b90cea",
+    "url": "/robofriends/static/js/1.3fe04a6d.chunk.js"
+  },
+  {
+    "revision": "22d5ba9cda52dc6f1bc6",
+    "url": "/robofriends/static/css/main.c67c557f.chunk.css"
+  },
+  {
+    "revision": "3fe04a6d5aa9c0b90cea",
+    "url": "/robofriends/static/css/1.c43e5dfa.chunk.css"
+  },
+  {
+    "revision": "ae7e530f83ccd8f9c02ab19a0e5bff02",
+    "url": "/robofriends/index.html"
+  }
+];
